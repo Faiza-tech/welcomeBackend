52011ccb02c04c19db3d24bc9579b234cbcad73f
Faiza <faraz_ul_islam@yahoo.com>
2024-08-01 13:40:04 +0100

    url added

--- src/index.js
+++ src/index.js
@@ -8,30 +8,38 @@ import cors from 'cors';
 // Set up express
 const app = express();
 app.disable('x-powered-by');
-app.use(cors());
+//app.use(cors());
+// CORS configuration
+const corsOptions = {
+    origin: 'https://welcomeabroad.netlify.app', // Replace with your Netlify site URL
+    methods: ['GET', 'POST'],
+    allowedHeaders: ['Content-Type'],
+  };
+  app.use(cors(corsOptions));
+
 
 app.use(express.json());
-
 app.use(express.urlencoded({ extended: true }));
 
 
 
-
+// Import and use your routes
 // server.js
 import registerRouter from "./server/router/user.js";
-
 
 app.use('/user', registerRouter);
 
 
 
-
+// Catch-all route
 app.get('*', (req, res) => {
     res.json({ ok: true });
 });
 
 
-const port = process.env.PORT;
+// Start server
+const port = process.env.PORT || 3030;
+//const port = process.env.PORT;
 app.listen(port, () => {
     console.log(`\n Server is running on http://localhost:${port}\n`);
 });
